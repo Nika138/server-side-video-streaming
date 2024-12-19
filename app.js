0c2133be2a1420d42f6a6116992b6bb29758e8cf
nikusha123456 <nikab6264@gmail.com>
2024-12-19 20:38:27 +0400

    starting development

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import { engine } from "express-handlebars";
+// import videoRoutes from "./routes/videoRoutes.js";
+// import authRoutes from "./routes/authRoutes.js";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/videos", videoRoutes);
+// app.use("/auth", authRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("Page not Found");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+export default app;
---
+++ routes/videoRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+// import videoController from "../controllers/videoController.js";
+
+const router = express.Router();
+
+// router.get("/:videoName", videoController.streamVideo);
+
+export default router;
